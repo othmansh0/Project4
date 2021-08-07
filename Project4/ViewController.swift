932804f2fdf7504f83a7467f8ae20754e9d00038
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-08-07 17:02:07 +0300

    Add open BarButtonItem

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -25,11 +25,37 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
-
-
+    }
+    
+    
+    
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.con", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com" , style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Canel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(ac,animated: true)
+        
+        
+    }
+    
+    func openPage(action: UIAlertAction){
+        guard let actionTitle = action.title else {return}
+        guard let url = URL(string: "https://" + actionTitle) else {return}
+        
+        webView.load(URLRequest(url: url))
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
-
+    
+    
 }
 
